f1073fe16c8f0320ffcfc394ca0fd32279499e9a
MateusCPires <162335957+MateusCPires@users.noreply.github.com>
2024-03-19 12:26:39 -0300

    Update frmSuperCalc.cs

--- frmCalculos/frmSuperCalc.cs
+++ frmCalculos/frmSuperCalc.cs
@@ -18,33 +18,128 @@ namespace frmCalculos
             InitializeComponent();
         }
 
+        private decimal dc;
+        private string vop, Operacao;
+        private Boolean Limpa;
+
         private void frmSuperCalc_KeyDown(object sender, KeyEventArgs e)
         {
+            
             if (e.KeyCode == Keys.Return)
             {
                 Close();
             }
+            Button Bot = new Button();
+            string tecla = e.KeyCode.ToString();
+            if(e.KeyCode >= Keys.NumPad0 && e.KeyCode <= Keys.NumPad9)
+            {
+                Bot.Text = tecla.Substring(6, 1);
+                f_numeros(Bot, e);
+            }
+            else
+            {
+                switch(e.KeyCode.ToString())
+                {
+                    case "Add":
+                        {
+                            Bot.Text = "+";
+                            f_operacoes(Bot, e);
+                            break;
+                        }
+                    case "Subtract":
+                        {
+                            Bot.Text = "-";
+                            f_operacoes(Bot, e);
+                            break;
+                        }
+                    case "Divide":
+                        {
+                            Bot.Text = "/";
+                            f_operacoes(Bot, e);
+                            break;
+                        }
+                    /*case "Add":
+                        {
+                            Bot.Text = "+";
+                            f_operacoes(Bot, e);
+                            break;
+                        }*/
+                    case "Return":
+                        {
+                            btnIgual_Click(sender, e);
+                            break;
+                        }
+                }
+            }
         }
 
-        private void Numeros(object sender, EventArgs e)
+        private void f_numeros(object sender, EventArgs e)
         {
+            lblVisor.Text = "";
+            Limpa = false;
             string numero = ((Button)sender).Text;
+            ((Button)sender).BackColor = Color.Bisque;
             if (lblVisor.Text == "0") lblVisor.Text = "";
             
             if(numero != "," || (numero == "," && !lblVisor.Text.Contains(",")))
             {
                 lblVisor.Text += numero;
             }
-
+            this.Focus();
         }
-
-        private decimal dc;
-        private string vop;
-        private Boolean vlimpa;
 
         private void f_operacoes(object sender, EventArgs e)
         {
+            decimal NumAtual = decimal.Parse(lblVisor.Text);
+            Operacao = ((Button)sender).Text;
+            decimal NumAnt = decimal.Parse(lblHist.Text);
+            if (Operacao == "+")
+            {
+                lblHist.Text += " " + NumAtual + " + ";
+                lblVisor.Text = "";
+                Limpa = true;
+            }
+            else if (Operacao == "-")
+            {
+                lblHist.Text += " " + NumAtual + " - ";
+                lblVisor.Text = "";
+                Limpa = true;
+            }
+        }
 
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            decimal NumAtual = decimal.Parse(lblVisor.Text);
+            Operacao = ((Button)sender).Text;
+            decimal NumAnt = decimal.Parse(lblHist.Text);
+
+            switch (Operacao)
+            {
+                case "+":
+                    {
+                        lblVisor.Text = (NumAnt + NumAtual).ToString();
+                        lblHist.Text += NumAtual + " = ";
+                        break;
+                    }
+                case "-":
+                    {
+                        lblVisor.Text = (NumAnt - NumAtual).ToString();
+                        lblHist.Text += NumAtual + " = ";
+                        break;
+                    }
+                case "*":
+                    {
+                        lblVisor.Text = (NumAnt * NumAtual).ToString();
+                        lblHist.Text += NumAtual + " = ";
+                        break;
+                    }
+                case "/":
+                    {
+                        lblVisor.Text = (NumAnt / NumAtual).ToString();
+                        lblHist.Text += NumAtual + " = ";
+                        break;
+                    }
+            }
         }
     }
 }
